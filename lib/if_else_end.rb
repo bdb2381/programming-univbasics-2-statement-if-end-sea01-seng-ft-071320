665889c770faeca496142de4599b9a34d598dbfd
Brad Beggs <brad.beggs@gmail.com>
2020-06-20 21:09:47 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,10 @@
-# Write your solution here
-if true
+current_time = Time.now
+
+if current_time % 2 == 0   #if current time is an even number 
+  #his can be a handy way to tell if a number is even or odd - if a number is evenly divisible by two, it is even.
   puts "This code runs!"
+
+else 
+  puts "Odd!"
+
 end
